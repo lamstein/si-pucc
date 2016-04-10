d5e97db401bfd8511698f6db51c6cc6727a186a3
Vinícius Barbutti <Vincius Barbutti>
2016-04-10 20:01:42 -0300

    inclusão das classe pilha e fila

--- 3SEM/ProjetoIntegradoC/Projeto2/Fila.java
+++ 3SEM/ProjetoIntegradoC/Projeto2/Fila.java
@@ -1,5 +1,147 @@
-/**
- * Created by rafael on 4/9/16.
- */
-public class Fila {
+public class Fila <X> implements Cloneable
+{
+    protected Object [] vetFila;
+    protected int inicio;
+    protected int fim;
+    protected int quantidade;
+    protected int tamanho;
+    
+    //Construtor para inicializar a fila;
+    public Fila (int tam) throws Excepetion
+    {
+        if(tam <= 0)
+            throws new Exception("Tamanho inválido.");
+        
+        vetFila = new Object[tam];
+        this.inicio = 0;
+        this.fim = 0;
+        this.quantidade = 0;
+        this.tamanho = tam;
+    }
+    
+    public void push (X n) throws Exception
+    {
+        if(this.quantidade == this.tamanho)
+            throw new Exception ("Fila cheia");
+        
+        if(this.fim > this.vetFila.length - 1)
+            this.fim = 0;
+            
+        if(n instanceof Cloneable){
+            Class classe = n.getClass();
+            Class<?> [] parmsFormais = null;
+            Method metodo = classe.getMethod("clone", parmsFormais);
+            Object [] parmsReais = null;
+            this.vetFila[this.fim] = (X)metodo.invoke(n,parmsReais);
+            this.fim++;
+            this.quantidade++; 
+        }
+        else{
+            this.vetFila[this.fim] = n;
+            this.fim++;
+            this.quantidade++;   
+        }
+    }
+    
+    //POR FAVOR REVISAR LÓGICA
+    public X pop () throws Exception
+    {
+        if(this.quantidade == 0)
+            throw new Exception("Fila vazia");
+        
+        if(this.inicio == this.quantidade)
+            this.inicio = 0;
+            
+        this.inicio++;
+        this.quantidade--;
+        return this.vetFila[this.inicio - 1];
+    }
+    
+    public boolean equals (Object obj)
+    {
+        if(this==obj)
+            return true; 
+            
+        if(obj == null)
+            return false;
+            
+        if(!obj instanceof Fila)
+            return false;
+        
+        Fila aux = (Fila)obj;
+        
+        if(this.inicio != aux.inicio)
+            return false;
+        
+        if(this.fim != aux.fim)
+            return false;
+            
+        if(this.quantidade != aux.quantidade)
+            return false;
+            
+        for(int i=0;i<this.ultimo;i++)
+            if(!this.vetFila[i].equals(aux.vetFila[i]))
+                return false;
+                
+        return true;
+    }
+    
+    public String toString()
+    {
+        String s = null;
+        
+        for(int i =0; i<this.quantidade;i++)
+            s = s + "[" + this.vetFila[i] + "]";
+        
+        return s;
+    }
+    
+    public int hashCode()
+    {
+        int res = super.hashCode();
+        
+        res = res*13 + new Integer (this.inicio).hashCode();
+        res = res*13 + new Integer (this.fim).hashCode();
+        res = res*13 + new Integer (this.quantidade).hashCode();
+        res = res*13 + new Integer (this.tamanho).hashCode();
+                
+        for(int i=0;i<this.quantidade;i++)
+            res = res*13 + new Integer (this.vetFila[i]).hashCode();
+        
+        return res;
+    }
+    
+    public Fila (Fila modelo) throws Excepetion
+    {
+        if(modelo==null)
+            throw new Exception ("Modelo null");
+            
+        this.vetFila = new Object[modelo.vetFila.length];
+        
+        for(int i = 0;i<modelo.quantidade;i++)
+        {
+            if(modelo.vetFila[i] instanceof Cloneable)
+                this.vetFila[i] = modelo.vetFila[i].clone();
+            else
+                this.vetFila[i] = modelo.vetFila[i];
+        }
+        
+        this.inicio = modelo.inicio;
+        this.fim = modelo.fim;
+        this.quantidade = modelo.quantidade;
+        this.tamanho = modelo.tamanho;
+    }
+    
+    public Object clone()
+    {
+        Fila f = null;
+        
+        try{
+            f = new Fila(this);
+        }
+        catch
+        {}
+        
+        return f;
+    }
 }
--- 3SEM/ProjetoIntegradoC/Projeto2/Pilha.java
+++ 3SEM/ProjetoIntegradoC/Projeto2/Pilha.java
@@ -1,5 +1,121 @@
-/**
- * Created by rafael on 4/9/16.
- */
-public class Pilha {
+public class Pilha <X> implements Cloneable
+{
+    protected Object [] vetPilha;
+    protected int topo;
+    protected int tamanho;
+    
+    public Pilha (int tam) throws Exception
+    {
+        if(tam <= 0)
+            throw new Exception ("Tamanho invalido");
+            
+        this.tamanho = tam;
+        vetPilha = new Object[this.tamanho];
+        this.topo = 0;
+    }
+    
+    public void push (X n) throws Exception
+    {
+        if(this.topo == this.tamanho)
+            throw new Exception ("Pilha cheia");
+        
+        if(n instanceof Cloneable){
+            Class classe = n.getClass();
+            Class<?>[] parmsFormais = null; //nao possui parametros formais
+            Method metodo = classe.getMethod("clone",  parmsFormais);
+            Object [] parmsReais = null;//nao possui parametros reais
+            this.vetPilha[this.topo] = (X)metodo.invoke(n, parmsReais);
+        }
+        else{
+            this.vetPilha[this.topo] = n;
+            this.topo++;   
+        }
+    }
+    
+    public X pop () throws Exception
+    {
+        if(this.topo == 0)
+            throw new Exception ("Pilha vazia");
+            
+        this.topo--;
+        return this.vetPilha[this.topo + 1];
+    }
+    
+    public boolean equals (Object obj)
+    {
+        if(this==obj)
+            return true;
+      
+        if(obj==null)
+            return false;
+        
+        if(!obj instanceof Pilha)
+            return false;
+            
+        Pilha aux = (Pilha)obj;
+        
+        if(this.topo != aux.topo)
+            return false;
+        
+        if(this.tamanho != aux.tamanho)
+            return false;
+            
+        for(int i=0;i<=this.topo;i++)
+            if(this.vetPilha[i] != aux.vetPilha[i])
+                return false;
+    }
+    
+    public String toString ()
+    {
+        String s = null;
+        
+        for(int i=0;i<this.tamanho;i++)
+            s = s + "[" + this.vetPilha[i] + "]";
+            
+        return s;
+    }
+    
+    public int hashCode()
+    {
+        int h = super.hashCode();
+        
+        for(int i=0;i<this.tamanho;i++)
+            h = h*13 + Integer new (this.vetPilha[i]).hashCode();
+            
+        h = h*13 +  Integer new(this.topo).hashCode();
+        h = h*13 +  Integer new(this.tamanho).hashCode();
+        
+        return h;
+    }
+    
+    public Pilha (Pilha modelo) throws Exception
+    {
+        if(modelo == null)
+            throw Exception ("Modelo não fornecido");
+            
+        this.vetPilha = new Object [modelo.tamanho];
+        
+        for(int i=0;i<modelo.tamanho;i++){
+            if(modelo.vetPilha[i] instanceof Cloneable)
+                this.vetPilha[i] = modelo.vetPilha[i].clone();
+            else
+                this.vetPilha[i] = modelo.vetPilha[i];
+        }
+        
+        this.topo = modelo.topo;
+        this.tamanho = modelo.tamanho;
+    }
+    
+    public Object clone()
+    {
+        Pilha p = null;
+        
+        try{
+            p = new Pilha (this);
+        }
+        catch(Exception erro)
+        {}
+        
+        return p;
+    }
 }
